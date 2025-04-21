5390a79581f97c042e704acbab72b32ed1a0d5d6
Mega Emmerson Ndobe <177267084+MegaEmerson@users.noreply.github.com>
2025-04-21 04:39:54 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+
+// C++ program to generate a random value and let a user guess whether the number generated even or odd
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    // random number generator
+    srand(time(0));
+
+    // Generate a random number between 1 and 1000
+    int x = rand() % 1000 + 1;
+
+    string Userguess;
+    //promt a user to enter the guess
+    cout << "Guess if the number generated even or odd? (Type 'EVEN' or 'ODD'): ";
+    cin >> Userguess;
+
+    //Finding out whether the guess is even or odd
+    string Random = (x % 2 == 0) ? "even" : "odd";
+
+    //displaying the random genarated number
+    cout << "The Genarated number is: " << x << endl;
+
+    // Check if the guess is correct
+    if (Userguess == Random) {
+        cout << "Your Guess is Correct!" << endl;
+    } else {
+        cout << "Your Guess is Incorrect!" << endl;
+    }
+
+    return 0;
+}
